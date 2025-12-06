d5f969678ec1cff06a1dae6c17fa90fafc0f6fec
Avi0n <14863961+Avi0n@users.noreply.github.com>
2025-12-05 23:22:03 -0800

    Start fresh

---
+++ PocketMesh/PocketMeshTests/PocketMeshTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketMeshTests.swift
+//  PocketMeshTests
+//
+//  Created by Nate Chung on 12/5/25.
+//
+
+import Testing
+@testable import PocketMesh
+
+struct PocketMeshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
