ec13496e03a123e0c38ebad3bc5d7090203cecde
Avi0n <14863961+Avi0n@users.noreply.github.com>
2026-01-10 20:09:57 -0800

    refactor: modernize sharing with ShareLink and fix iPad popover
    Replace UIActivityViewController with SwiftUI ShareLink in LineOfSightView
    for cleaner, more idiomatic SwiftUI code. Fix iPad crash by adding proper
    popover source configuration in AboutSection.

--- PocketMesh/Views/Settings/Sections/AboutSection.swift
+++ PocketMesh/Views/Settings/Sections/AboutSection.swift
@@ -87,6 +87,16 @@ struct AboutSection: View {
                     while let presented = topVC.presentedViewController {
                         topVC = presented
                     }
+                    if let popover = activityVC.popoverPresentationController {
+                        popover.sourceView = topVC.view
+                        popover.sourceRect = CGRect(
+                            x: topVC.view.bounds.midX,
+                            y: topVC.view.bounds.midY,
+                            width: 0,
+                            height: 0
+                        )
+                        popover.permittedArrowDirections = []
+                    }
                     topVC.present(activityVC, animated: true)
                 }
 
