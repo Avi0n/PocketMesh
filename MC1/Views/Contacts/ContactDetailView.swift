a5c570f8264848b337f0d91277eb956694a1e0c3
Avi0n <14863961+Avi0n@users.noreply.github.com>
2026-03-14 23:41:04 -0700

    fix(style): use Color() shorthand instead of Color(uiColor:) for system color

--- MC1/Views/Contacts/ContactDetailView.swift
+++ MC1/Views/Contacts/ContactDetailView.swift
@@ -755,7 +755,7 @@ private struct ContactLocationSection: View {
             }
             .listRowBackground(
                 UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10)
-                    .fill(Color(uiColor: .secondarySystemGroupedBackground))
+                    .fill(Color(.secondarySystemGroupedBackground))
             )
 
             // Open in Maps
